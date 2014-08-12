f65366856f6b5519d4238ff327701445bbce39b6
nejlaguner <nejlaguner@gmail.com>
2014-08-12 17:30:09 +0300

    form türüne göre içerik doldurma fonksiyonları yazıldı

--- ACKSiparsTakip.Business/ACKBusiness/SiparisIslemleriBS.cs
+++ ACKSiparsTakip.Business/ACKBusiness/SiparisIslemleriBS.cs
@@ -10,17 +10,34 @@ namespace ACKSiparisTakip.Business.ACKBusiness
     [ServiceConnectionNameAttribute("ACKconnectionString")]
     public class SiparisIslemleriBS : BusinessBase
     {
-        public DataTable KapıModeliGetir()
+        public DataTable KapıModeliGetir(Dictionary<string, object> prms)
         {
             DataTable dt = new DataTable();
             IData data = GetDataObject();
 
-
-            string sqlText = @"SELECT [ID],[KAPITURADI]  FROM KAPI_TUR";
-
+            data.AddSqlParameter("ID", prms["ID"], SqlDbType.VarChar, 50);
+     
+            string sqlText = @"SELECT KAPIMODELID,AD FROM REF_KAPIMODEL WHERE KAPISERIID=@ID";
             data.GetRecords(dt, sqlText);
+
             return dt;
         }
+
+        public DataTable KapıRenkGetir(Dictionary<string, object> prms)
+        {
+            DataTable dt = new DataTable();
+            IData data = GetDataObject();
+            string durum = "1";
+           
+            data.AddSqlParameter("KAPISERI", prms["KAPISERI"], SqlDbType.VarChar, 50);
+            data.AddSqlParameter("DURUM",durum, SqlDbType.VarChar, 50);
+
+            string sqlText = @"SELECT KAPIRENKID,AD FROM REF_KAPIRENK WHERE "+prms["KAPISERI"].ToString()+"=@DURUM";
+            data.GetRecords(dt, sqlText);
+
+            return dt;
+        }
+
 
         
     }
